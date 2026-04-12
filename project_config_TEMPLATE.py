f5b27f447b1c131af0c59b42f1f95928655c1c54
NAPHthalene130 <1301537275@qq.com>
2026-04-12 22:44:14 +0800

    feat:更新参考配置模板

--- project_config_TEMPLATE.py
+++ project_config_TEMPLATE.py
@@ -1,13 +1,9 @@
-# 大模型服务配置
 BASE_URL = "https://ark.cn-beijing.volces.com/api/v3"
+EMBEDDING_URL = "https://ark.cn-beijing.volces.com/api/v3/embeddings/multimodal"
 API_KEY = "<APIKEY>"
-Lite_Model = "doubao-seed-2-0-pro-260215"
-Flash_Model = "doubao-seed-2-0-pro-260215"
+Lite_Model = "doubao-seed-2-0-mini-260215"
+Flash_Model = "doubao-seed-2-0-lite-260215"
 Pro_Model = "doubao-seed-2-0-pro-260215"
 Embedding_Model = "doubao-embedding-vision-251215"
-# JWT 签名密钥
-JWT_SECRET_KEY = "<JWT_SECRET_KEY>"
-NEO4J_URI = "neo4j://127.0.0.1:7687"
-NEO4J_USERNAME = "neo4j"
-NEO4J_PASSWORD = "<NEO4J_PASSWORD>"
-NEO4J_DATABASE = "neo4j"
+
+JWT_SECRET_KEY = "JWT_SECRET_KEY"
